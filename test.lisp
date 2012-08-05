7fd2fc44159ab491c1ceaab6ca197d443be1fda8
Francisco Soto <ebobby@gmail.com>
2012-08-05 03:03:20 -0700

    Added some tests.

---
+++ test.lisp
@@ -0,0 +1,34 @@
+;;; General purpose in-memory B+ tree.
+;;; Copyright (c) 2012, Francisco Soto All rights reserved (see COPYING file for details).
+
+(in-package :bplustree)
+
+;; Builds a tree with random
+(defun build-test-tree (order n &key (random-max 99999999999999999999))
+  "Builds a tree with the given order and at most n elements with random records.
+   Keys are numbers and the records are the same as the keys but coerced to strings.
+   Also returns an a-list of the inserted key/records."
+  (let ((items (loop
+                  for i from 1 to n
+                  for key = (random random-max)
+                  for record = (write-to-string key)
+                  collect (list key record))))
+    (values (apply #'bplustree-insert-many
+                   (bplustree-new order)
+                   items)
+            items)))
+
+(defun test-bplustree-insert-search (order n)
+  "Builds a tree of the given order with at most n random elements, then a test is run trying to see
+   if every element insterted is searchable."
+  (multiple-value-bind (tree items) (build-test-tree order n)
+    (loop
+       for i in items
+       for key = (car i)
+       for record = (cadr i)
+       do (format t "Searching key ~a : " key)
+         (if (string= (bplustree-search key tree) record)
+             (format t "found.~%")
+             (progn
+               (format t "not found.~%")
+               (return))))))
